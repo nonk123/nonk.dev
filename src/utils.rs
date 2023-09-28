25c84d59028bc089a9ac1a2663c54beea3bb8765
nonk123 <me@nonk.dev>
2023-09-29 00:29:53 +0300

    Minor fixups

--- src/utils.rs
+++ src/utils.rs
@@ -5,7 +5,7 @@ use seed::{prelude::*, *};
 pub fn icon_link(icon_group: &str, icon_name: &str, url: &str) -> Node<Msg> {
     a![
         C!["link"],
-        attrs! { At::Href => url },
+        attrs! { At::Href => url, At::Target => "_blank", },
         icon(icon_group, icon_name),
     ]
 }
