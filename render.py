941f0f99b9b2c1ea1903da0f7106c17646805971
nonk123 <me@nonk.dev>
2025-03-28 17:18:57 +0300

    Add the last updated date

--- render.py
+++ render.py
@@ -1,12 +1,17 @@
 import os
 import shutil
 import json
+import time
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 global env, blog, OUT_PATH
 
 OUT_PATH = "out"
+
+SHARED_CTX = {
+    "last_updated": time.strftime("%Y-%m-%d", time.gmtime()),
+}
 
 if os.path.exists(OUT_PATH):
     for child in os.listdir(OUT_PATH):
@@ -23,19 +28,20 @@ os.mkdir(os.path.join(OUT_PATH, "blog"))
 shutil.copytree("assets", os.path.join(OUT_PATH, "assets"))
 shutil.copy("robots.txt", OUT_PATH)
 
-env = Environment(
-    loader=FileSystemLoader("templates"),
-    autoescape=select_autoescape()
-)
+env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
 
-def render(path, ctx = {}):
+
+def render(path, ctx={}):
+    ctx = {**SHARED_CTX, **ctx}
     out_path = os.path.join(OUT_PATH, path).rsplit(".", 1)[0]
     env.get_template(path).stream(ctx).dump(out_path)
+
 
 with open("blog/index.json", "rt") as fp:
     blog = json.load(fp)
 
-def render_article(id, index = False):
+
+def render_article(id, index=False):
     article = blog["articles"][id]
 
     with open(os.path.join("blog", id + ".txt"), "rt") as fp:
@@ -43,10 +49,18 @@ def render_article(id, index = False):
 
     paragraphs = contents.strip().split("\n\n")
     fmt_articles = [{"id": id, **article} for id, article in blog["articles"].items()]
-    ctx = {"id": id, **article, "paragraphs": paragraphs, "articles": fmt_articles}
+
+    ctx = {
+        **SHARED_CTX,
+        "id": id,
+        **article,
+        "paragraphs": paragraphs,
+        "articles": fmt_articles,
+    }
 
     out_path = os.path.join(OUT_PATH, "blog", "index.html" if index else id + ".html")
     env.get_template("article.html.j2").stream(ctx).dump(out_path)
+
 
 render("index.html.j2")
 render_article(blog["default"], True)
