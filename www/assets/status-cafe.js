d2804fb4ef34f94df3c6fdf566f54f8aa1cb2c69
nonk123 <me@nonk.dev>
2025-05-21 21:59:41 +0300

    Nitpick at JS

--- www/assets/status-cafe.js
+++ www/assets/status-cafe.js
@@ -1,26 +1,28 @@
-fetch("https://status.cafe/users/nonk/status.json")
-    .then((r) => r.json())
-    .then(cafe);
+(function () {
+    fetch("https://status.cafe/users/nonk/status.json")
+        .then((r) => r.json())
+        .then(cafe);
 
-function cafe(response) {
-    const usernameLink = document.createElement("a");
-    usernameLink.href = "https://status.cafe/users/nonk";
-    usernameLink.target = "_blank";
-    usernameLink.textContent = response.author;
+    function cafe(response) {
+        const usernameLink = document.createElement("a");
+        usernameLink.href = "https://status.cafe/users/nonk";
+        usernameLink.target = "_blank";
+        usernameLink.textContent = response.author;
 
-    const username = document.getElementById("statuscafe-username");
-    username.appendChild(usernameLink);
+        const username = document.getElementById("statuscafe-username");
+        username.appendChild(usernameLink);
 
-    const content = document.getElementById("statuscafe-content");
-    if (!response.content.length) {
-        content.textContent = "No status yet.";
-        return;
+        const content = document.getElementById("statuscafe-content");
+        if (!response.content.length) {
+            content.textContent = "No status yet.";
+            return;
+        }
+
+        const marginalia = document.createTextNode(
+            " " + response.face + " " + response.timeAgo
+        );
+        username.appendChild(marginalia);
+
+        content.innerHTML = response.content;
     }
-
-    const marginalia = document.createTextNode(
-        " " + response.face + " " + response.timeAgo
-    );
-    username.appendChild(marginalia);
-
-    content.innerHTML = response.content;
-}
+})();
