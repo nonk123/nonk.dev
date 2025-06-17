1c31d36f92ebdfd3be3025eea035029726fd8430
nonk123 <me@nonk.dev>
2025-06-17 23:13:11 +0300

    Declutter shitposts directory

--- www/shitposts/render.lua
+++ www/shitposts/render.lua
@@ -1,8 +1,2 @@
 local posts = json("shitposts/index.json");
-local index = { ["posts"] = posts };
-
-for _, value in pairs(posts) do
-    value.href = "/shitposts/" .. value.file;
-end
-
-render("shitposts/_index.html", "shitposts/index.html", index);
+render("shitposts/_index.html", "shitposts/index.html", { ["posts"] = posts });
