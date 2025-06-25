f55b603216909c2179f3ab5bc9c31450b9bbd197
nonk123 <me@nonk.dev>
2025-06-25 16:59:27 +0300

    Screenshot in desktop/mobile resolution

--- trash/capture.js
+++ trash/capture.js
@@ -1,19 +1,39 @@
 const browsers = require("playwright");
+
 const paths = ["/", "/projects", "/about"];
+const viewports = [
+    {
+        name: "landscape",
+        width: 1366,
+        height: 768,
+    },
+    {
+        name: "portrait",
+        width: 828,
+        height: 1792,
+    },
+];
 
 (async () => {
     const browser = await browsers.chromium.launch();
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.setViewportSize({ width: 1366, height: 768 });
 
     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
     for (const idx in paths) {
         await page.goto(`https://nonk.dev${paths[idx]}`);
         await page.waitForEvent("requestfinished");
 
-        await delay(1000);
-        await page.screenshot({ path: `.github/assets/screenie-${idx}.png` });
+        for (const viewport of viewports) {
+            await page.setViewportSize({
+                width: viewport.width,
+                height: viewport.height,
+            });
+            await delay(1000);
+            await page.screenshot({
+                path: `.github/assets/screenie-${viewport.name}-${idx}.png`,
+            });
+        }
     }
 
     await browser.close();
