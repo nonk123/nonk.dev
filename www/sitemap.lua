f0701fccdd8f8452c91de80ad51d081aea3dbf72
nonk123 <me@nonk.dev>
2025-07-12 12:04:31 +0300

    Goofier ass sitemap generation

--- www/sitemap.lua
+++ www/sitemap.lua
@@ -1,1 +1,14 @@
-render("_sitemap.xml", "sitemap.xml", json("blog/index.json"));
+local sections = { "/", "/about", "/now", "/projects", "/shitposts", "/guestbook" };
+local realsections = {}
+
+for _, path in pairs(sections) do
+    table.insert(realsections, {
+        path = path,
+        lastmod = lastmod(path:sub(2)),
+    });
+end
+
+render("_sitemap.xml", "sitemap.xml", {
+    blog = json("blog/index.json"),
+    sections = realsections,
+});
