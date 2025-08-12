2f2f7b8d81a9794ae274729483f81a2ebdb51724
nonk123 <me@nonk.dev>
2025-08-12 13:48:39 +0300

    Inline shitposts index into a Lua table

--- www/shitposts/render.lua
+++ www/shitposts/render.lua
@@ -1,1 +1,18 @@
-render("shitposts/_index.html", "shitposts/index.html", { posts = json("shitposts/index.json") });
+local posts = {
+    { "gcc.mp4",             "the GCC conspiracy" },
+    { "gromitjockey.mp4",    "a shocking revelation from wallace & gromit" },
+    { "bigtasty.mp4",        "how we cook the big tasty" },
+    { "postal.mp4",          "postal dude irl" },
+    { "arbus.mp4",           "arbus arbus privet!!!" },
+    { "THANG.mp4",           "mcpickle pounder......" },
+    { "NOTTHETRUCK.mp4",     "NOT THE TRUCK!!!!" },
+    { "apartmentmd.mp4",     "a certified house md moment" },
+    { "mustard.mp4",         "the origin story of pickles and mustard" },
+    { "croatian.mp4",        "PATRICK BATEMAN??? no PATRICK BATEMIČ" },
+    { "gucci.mp4",           "me when $1 aliexpress shorts" },
+    { "kiryu.mp4",           "we've hit a new low" },
+    { "familyguyreal3d.mp4", "family guy plot twist" },
+    { "danceskillz.mp4",     "cousin's bestie breaking it" },
+    { "truegaming.mp4",      "cousin gaming" },
+}
+render("shitposts/_index.html", "shitposts/index.html", { posts = posts });
