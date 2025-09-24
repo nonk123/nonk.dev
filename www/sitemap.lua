786708ac2b69b88dce57a16436109272c20bba2b
nonk123 <me@nonk.dev>
2025-09-24 10:57:18 +0300

    Update sitemap sections and prune hidden articles

--- www/sitemap.lua
+++ www/sitemap.lua
@@ -1,14 +1,12 @@
-local sections = { "/", "/about", "/now", "/projects", "/shitposts", "/guestbook" };
-local realsections = {}
-
-for _, path in pairs(sections) do
-    table.insert(realsections, {
+local sections = { "/", "/about", "/now", "/projects", "/blog", "/guestbook", "/shitposts" };
+for idx, path in pairs(sections) do
+    sections[idx] = {
         path = path,
         lastmod = lastmod(path:sub(2)),
-    });
+    };
 end
 
 render("_sitemap.xml", "sitemap.xml", {
     blog = json("blog/index.json"),
-    sections = realsections,
+    sections = sections,
 });
