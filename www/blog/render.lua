aa5c16791e8a787ce79fe9428dc2f0da74d2da29
nonk123 <me@nonk.dev>
2025-10-11 20:42:21 +0300

    Test `required` by adding `meta` descriptions for blog articles

--- www/blog/render.lua
+++ www/blog/render.lua
@@ -1,28 +1,33 @@
-local index = json("blog/index.json");
+local index = json("blog/index.json")
 
 for id, article in pairs(index.articles) do
-    local contents = read("blog/md/" .. id .. ".txt");
+    local contents = read("blog/md/" .. id .. ".txt")
     local paragraphs = {}
 
-    contents = contents:gsub("(:?^|\n) +([^\n]+)", "%1");
-    contents = contents:gsub("([^\n])\n([^\n])", "%1 %2");
+    contents = contents:gsub("(:?^|\n) +([^\n]+)", "%1")
+    contents = contents:gsub("([^\n])\n([^\n])", "%1 %2")
 
     for paragraph in contents:gmatch("[^%c]+") do
-        table.insert(paragraphs, paragraph);
+        table.insert(paragraphs, paragraph)
     end
 
-    local ctx = {
+    local ctx = {}
+    local addendum = {
         id = id,
         default = index.default,
-        date_pub = article.date_pub,
         date_mod = article.date_mod or article.date_pub,
-        title = article.title,
         paragraphs = paragraphs,
         articles = index.articles,
-    };
+    }
+    for k, v in pairs(article) do
+        ctx[k] = v
+    end
+    for k, v in pairs(addendum) do
+        ctx[k] = v
+    end
 
-    render("blog/_article.html", "blog/" .. id .. "/index.html", ctx);
+    render("blog/_article.html", "blog/" .. id .. "/index.html", ctx)
     if id == index.default then
-        render("blog/_article.html", "blog/index.html", ctx);
+        render("blog/_article.html", "blog/index.html", ctx)
     end
 end
