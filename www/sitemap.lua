7916c92a29aeeaed10167d6f8beba6a13e0e2bc8
nonk123 <me@nonk.dev>
2025-10-11 20:48:40 +0300

    Silence stupid Lua langserver

--- www/sitemap.lua
+++ www/sitemap.lua
@@ -1,5 +1,7 @@
-local sections = { "/", "/about", "/now", "/projects", "/blog", "/guestbook", "/shitposts" }
-for idx, path in pairs(sections) do
+local section_names = { "/", "/about", "/now", "/projects", "/blog", "/guestbook", "/shitposts" }
+local sections = {}
+
+for idx, path in pairs(section_names) do
     sections[idx] = {
         path = path,
         lastmod = lastmod(path:sub(2)),
