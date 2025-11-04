9315541a79532ef237f48fcadbeb1b71ecf0283f
nonk123 <me@nonk.dev>
2025-11-04 23:13:51 +0300

    Inline `nav.json` into a Lua table

--- www/nav.lua
+++ www/nav.lua
@@ -1,1 +1,9 @@
-inject("nav", json("nav.json"));
+inject("nav", {
+    { href = "/",          title = "home",      rel = "me" },
+    { href = "/about",     title = "about",     rel = "me" },
+    { href = "/now",       title = "now",       rel = "me" },
+    { href = "/projects",  title = "projects" },
+    { href = "/blog",      title = "blog",      hover = "under construction!" },
+    { href = "/guestbook", title = "guestbook", hover = "please sign!" },
+    { href = "/shitposts", title = "shitposts", hover = "very shitty..." },
+});
